45779cd89dfc1b045da407ff9be60f1d51f16262
Amogelang (Amo) Mabonela <mabonelaa@gmail.com>
2026-06-12 18:24:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/uzuri", {
     useNewUrlParser: true, 
     useUnifiedTopology: true,  
-  })
+  }) 
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log("Server running on port ${PORT"));
