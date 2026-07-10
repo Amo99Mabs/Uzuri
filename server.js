c1524af8f8c6ba9926ebb988aaf770206a905862
Amogelang (Amo) Mabonela <mabonelaa@gmail.com>
2026-07-10 08:26:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const productRoutes = require("./server/routes/products"); 
     
-const app = express();
+const app = express(); 
 const PORT = 5004;   
  
 app.use(cors());
